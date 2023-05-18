643280c447f1f3ba7b06af117b8a3dd27c0d1820
Nicholas Agwah <nagwah@teqbridgeltd.com>
2023-05-18 13:59:41 +0100

    context

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeContextProvider from "./context/ThemeContext";
 import "./App.css";
+import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./context/AuthContext";
 
 function App() {
 	return (
 		<>
 			<div>
 				<ThemeContextProvider>
-					<Navbar />
-					<BookList />
+					<AuthContextProvider>
+						<Navbar />
+						<BookList />
+						<ThemeToggle />
+					</AuthContextProvider>
 				</ThemeContextProvider>
 			</div>
 		</>
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,57 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../context/ThemeContext";
+import { AuthContext } from "../context/AuthContext";
 
 class BookList extends Component {
-	static contextType = ThemeContext;
+	// consuming context with class component restriction you use static contextType
+	// 	static contextType = ThemeContext;
 	render() {
-		const { isLightTheme, light, dark } = this.context;
-		const theme = isLightTheme ? light : dark;
 		return (
-			<div
-				className="book-list"
-				style={{ color: theme.syntax, background: theme.bg }}
-			>
-				<ul>
-					<li style={{ background: theme.ui }}>the way of kings</li>
-					<li style={{ background: theme.ui }}>
-						the name of the wind
-					</li>
-					<li style={{ background: theme.ui }}>the final empire</li>
-				</ul>
-			</div>
+			<AuthContext.Consumer>
+				{(authContext) => (
+					<ThemeContext.Consumer>
+						{(context) => {
+							const { isLightTheme, light, dark } = context;
+							const theme = isLightTheme ? light : dark;
+							const { isAuthenticated, toggleAuth } = authContext;
+							return (
+								<>
+									<div
+										className="book-list"
+										style={{
+											color: theme.syntax,
+											background: theme.bg,
+										}}
+									>
+										<ul>
+											<li
+												style={{ background: theme.ui }}
+											>
+												the way of kings
+											</li>
+											<li
+												style={{ background: theme.ui }}
+											>
+												the name of the wind
+											</li>
+											<li
+												style={{ background: theme.ui }}
+											>
+												the final empire
+											</li>
+										</ul>
+									</div>
+									<button onClick={toggleAuth}>
+										{isAuthenticated
+											? "LoggedIn"
+											: "LoggedOut"}
+									</button>
+								</>
+							);
+						}}
+					</ThemeContext.Consumer>
+				)}
+			</AuthContext.Consumer>
 		);
 	}
 }
