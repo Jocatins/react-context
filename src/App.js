61d814511e1120df602ea152c9a44c5ad4d6926d
Nicholas Agwah <nagwah@teqbridgeltd.com>
2023-05-18 09:38:00 +0100

    context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
+import BookList from "./components/BookList";
+import Navbar from "./components/Navbar";
+import ThemeContextProvider from "./context/ThemeContext";
 import "./App.css";
-import { AddItem } from "./components/AddItem";
-import { ShoppingList } from "./components/ShoppingList";
-import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
 	return (
 		<>
 			<div>
-				<GlobalProvider>
-					<AddItem />
-					<ShoppingList />
-				</GlobalProvider>
+				<ThemeContextProvider>
+					<Navbar />
+					<BookList />
+				</ThemeContextProvider>
 			</div>
 		</>
 	);
